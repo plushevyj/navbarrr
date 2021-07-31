d0829ffba26daa64a1b87fa86cbb522ad98c2dd7
plushevyj <65882973+plushevyj@users.noreply.github.com>
2021-07-31 19:16:04 +0300

    Add files via upload

---
+++ navbarrr/lib/navbar_bloc.dart
@@ -0,0 +1,25 @@
+import 'package:bloc/bloc.dart';
+import 'package:navbarrr/navbar_states.dart';
+
+enum NavbarItems { Red, Green, Blue }
+
+class NavbarBloc extends Bloc<NavbarItems, NavbarState> {
+  @override
+  NavbarState get initialState => ShowRed();
+
+  @override
+  Stream<NavbarState> mapEventToState(
+      NavbarState state, NavbarItems event) async* {
+    switch (event) {
+      case NavbarItems.Blue:
+        yield ShowBlue();
+        break;
+      case NavbarItems.Green:
+        yield ShowGreen();
+        break;
+      default:
+        yield ShowRed();
+        break;
+    }
+  }
+}
---
+++ navbarrr/lib/navbar_states.dart
@@ -0,0 +1,16 @@
+abstract class NavbarState {}
+
+class ShowRed extends NavbarState {
+  final String title = "Red";
+  final int itemIndex = 0;
+}
+
+class ShowBlue extends NavbarState {
+  final String title = "Blue";
+  final int itemIndex = 1;
+}
+
+class ShowGreen extends NavbarState {
+  final String title = "Green";
+  final int itemIndex = 2;
+}
